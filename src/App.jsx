312d5fd37eeef63a2a0d37f9087ad5bbad3e7a22
punz-ind <punariwork@gmail.com>
2026-08-07 14:47:52 +0700

    feat: Menambahkan QGIS Dataset Baru berupa Adm_Kota_Bandung; Kemudian menambahkan Layer Control untuk mengubah tampilan pada peta.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
 
 const app = () => {
   return (
@@ -7,13 +7,24 @@ const app = () => {
         <MapContainer
           center={[-6.916050781414965, 107.6281649298608]}
           zoom={13}
-          scrollWheelZoom={false}
+          // scrollWheelZoom={false}
           className="w-full h-[990px]"
         >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+          <LayersControl position="topright">
+            <LayersControl.BaseLayer checked name="OpenStreetMap">
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Google Satellite">
+              <TileLayer
+                attribution='&copy; <a href="https://www.google.com/maps">Google Maps</a> contributors'
+                url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                subdomains={["mt0", "mt1", "mt2", "mt3"]}
+              />
+            </LayersControl.BaseLayer>
+          </LayersControl>
         </MapContainer>
       </div>
     </div>
