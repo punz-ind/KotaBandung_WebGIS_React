d783a1f3f24e8f477a1932a119cdc96f49a0ab51
punz-ind <punariwork@gmail.com>
2026-08-10 07:56:05 +0700

    feat: Update data Adm Kota Bandung dan mengubah metode pembagian kelas densitas dari Equal Interval menjadi Natural Breaks (Jenks).

--- src/components/ui/MapComponent.jsx
+++ src/components/ui/MapComponent.jsx
@@ -1,19 +1,22 @@
 import { LayersControl, MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import dataKotaBandung from "../../assets/data/Adm_Kota_Bandung.json";
+// import { useState } from "react";
 
 const acuanDensitas = (densitas) => {
-  return densitas <= 11908
+  return densitas <= 8118
     ? "#4ade80"
-    : densitas > 11908 && densitas <= 18830
+    : densitas > 8118 && densitas <= 13501
       ? "#facc15"
-      : densitas > 18830 && densitas <= 25751
+      : densitas > 13501 && densitas <= 20960
         ? "#f97316"
-        : densitas > 25751
+        : densitas > 20960
           ? "#dc2626"
           : "#000";
 };
 
 const MapComponent = () => {
+  // const [OpenSidebar, setOpenSidebar] = useState(false);
+
   const styleKecamatan = {
     fillColor: "#2196F3",
     weight: 1,
@@ -34,6 +37,7 @@ const MapComponent = () => {
   const onEachKecamatan = (feature, layer) => {
     const kecamatan = feature.properties.WADMKC;
     const luasWilayah = feature.properties.LuasKm2;
+
     layer.bindPopup(`<strong>Kecamatan:</strong> ${kecamatan}<br/>
       <strong>Luas Wilayah:</strong> ${luasWilayah.toLocaleString("id-ID")} km²`);
     layer.on({
@@ -52,11 +56,15 @@ const MapComponent = () => {
 
   const onEachDensitas = (feature, layer) => {
     const kecamatan = feature.properties.WADMKC;
+    const luasWilayah = feature.properties.LuasKm2;
     const densitas = feature.properties.Densitas;
+    const populasi = feature.properties.Populasi;
     const keterangan = feature.properties.Keterangan;
     layer.bindPopup(
       `<strong>Kecamatan:</strong> ${kecamatan}<br/>
+      <strong>Luas Wilayah:</strong> ${luasWilayah.toLocaleString("id-ID")} km²<br/>
       <strong>Densitas:</strong> ${densitas.toLocaleString("id-ID")} jiwa/km²<br/>
+      <strong>Populasi:</strong> ${populasi.toLocaleString("id-ID")} jiwa<br/>
       <strong>Keterangan:</strong> ${keterangan}`,
     );
     layer.on({
